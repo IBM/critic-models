72312714614c3511474aafa06584bdc5e1311485
gililior <gili.lior96@gmail.com>
2025-01-07 16:45:43 +0200

    eval_steps back to 500

--- routing/base_learning_class.py
+++ routing/base_learning_class.py
@@ -127,7 +127,7 @@ class LLM_Classifier:
             eval_strategy="steps",
             save_strategy="steps",
             save_steps=1000,  # Save every 1000 steps
-            eval_steps=1,  # Evaluate every 500 steps
+            eval_steps=500,  # Evaluate every 500 steps
             logging_dir="./logs",
             logging_steps=100,  # Log every 100 steps
             learning_rate=self.learning_rate,
