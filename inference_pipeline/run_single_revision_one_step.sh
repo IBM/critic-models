d5225f1a45cc92c9312da9c89abcb510b7109180
gililior <gili.lior96@gmail.com>
2025-01-23 14:08:45 +0200

    add refine prompt to script

--- inference_pipeline/run_single_revision_one_step.sh
+++ inference_pipeline/run_single_revision_one_step.sh
@@ -27,4 +27,5 @@ python inference_pipeline/refine_model_response.py \
   --split $SPLIT \
   --responses $INIT_RESPONSES \
   --refine_model $MODEL \
-  --out_path $OUT_PATH
+  --out_path $OUT_PATH \
+  --refine_prompt "Your task is to generate text based on the provided task description. Additionally, you are given an example output from another model (the \"reference\"). Use the reference to identify elements that are well-executed and should be retained or adapted in your response, while also improving upon areas where it falls short or does not align with the task description. Focus on producing a result that best meets the requirements of the task and outperforms the reference. Do not provide any additional explanation, include only your answer to the task description.\n\nTask description: {instruction}\n\nReference: \"{ai_response}\"\n\nAnswer:"
