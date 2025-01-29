ae77bbc81fc4e7efe6f00090c3316dfa87596b80
gililior <gili.lior96@gmail.com>
2025-01-29 18:02:47 +0200

    use big gpu, if-eval

--- inference_pipeline/run_all_initial_response_generations.sh
+++ inference_pipeline/run_all_initial_response_generations.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --array=0-3
+#SBATCH --array=0-2
 #SBATCH --mem=40gb
 #SBATCH -c2
 #SBATCH --time=12:00:00
-#SBATCH --gres=gpu:1,vmem:24g
+#SBATCH --gres=gpu:1,vmem:40g
 #SBATCH --error=slurm_logs/slurm_%A_%a.err
 #SBATCH --output=slurm_logs/slurm_%A_%a.out
 #SBATCH --job-name=initial_generations
@@ -12,7 +12,7 @@
 
 #model_names=("meta-llama/Llama-3.2-1B-Instruct" "meta-llama/Llama-3.2-3B-Instruct" "meta-llama/Llama-3.1-8B-Instruct" "google/gemma-2-2b-it" "google/gemma-2-9b-it")
 #model_names=("meta-llama/Llama-3.2-3B-Instruct" "meta-llama/Llama-3.1-8B-Instruct" "google/gemma-2-9b-it")
-model_names=("Qwen/Qwen2.5-0.5B-Instruct" "Qwen/Qwen2.5-1.5B-Instruct" "Qwen/Qwen2.5-3B-Instruct", "Qwen/Qwen2.5-7B-Instruct")
+model_names=("Qwen/Qwen2.5-0.5B-Instruct" "Qwen/Qwen2.5-1.5B-Instruct" "Qwen/Qwen2.5-3B-Instruct")  # "Qwen/Qwen2.5-7B-Instruct")
 
 
 dataset="/cs/snapless/gabis/gililior/arena_data_final/constrained-lmsys-chat-1m"
