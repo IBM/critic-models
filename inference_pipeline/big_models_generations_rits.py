3ceff849ad5c4096dd78ba52734fb8f7362ee442
gililior <gili.lior96@gmail.com>
2025-02-04 14:06:57 +0200

    added the option for init generations in rits

---
+++ inference_pipeline/big_models_generations_rits.py
@@ -0,0 +1,119 @@
+import os
+from argparse import ArgumentParser
+from prepare_data.classify_constrained_generation_tasks import ConstrainedGenerationClassificationRITS
+from prepare_data.decompose_tasks import ArenaClassifiedData
+from utils.eval_consts import PROMPT_EVAL
+import random
+import json
+from multiprocessing import Pool, cpu_count
+from openai import OpenAI
+from ibm_watsonx_ai.metanames import GenTextParamsMetaNames as GenParams
+from tqdm import tqdm
+from generate_initial_response import InitialResponse
+from prepare_data.classify_constrained_generation_tasks import BaseDataset
+from datasets import load_dataset
+
+
+class OrigData(BaseDataset):
+
+    def __init__(self, name_or_path, split, tasks_key):
+        self.split = split
+        self.tasks_key = tasks_key
+        super().__init__(name_or_path)
+
+    def get_name_or_path(self):
+        return self.name_or_path
+
+    def load_data(self, name_or_path):
+        data = load_dataset(self.data, split=self.split)
+        return data
+
+    def get_tasks_list(self):
+        return list(self.data[self.tasks_key])
+
+
+class InitGenerationsRITS(ConstrainedGenerationClassificationRITS):
+
+    def __init__(self, model, data):
+        super().__init__(data, model, max_new_tokens=1000)
+
+    def get_name(self):
+        return f"init-generations-via-rits"
+
+    def get_out_path(self, out_dir):
+        dataset_name = self.dataset_name.split(os.sep)[-1].replace(".json", "")
+        path = os.path.join(out_dir, f"{self.get_name()}-{self.short_model_name}.{dataset_name}.json")
+        print(f"output path at {path}")
+        return path
+    
+    def dump_results(self, out_dir, all_scores):
+        out_path_dump = self.get_out_path(out_dir)
+        with open(out_path_dump, 'wt') as f:
+            str_feedback_dict = json.dumps(all_scores, indent=2)
+            f.write(str_feedback_dict)
+
+
+def generate_parallel(obj):
+    all_tasks = set(obj.data.get_tasks_list())
+    model_name = obj.model_name
+    api_key=obj.get_api_key()
+    base_url=obj.get_api_endpoint().format(obj.model_name_for_endpoint)
+    all_results = {}
+    all_args = {}
+    pool= Pool(cpu_count())
+    total=0
+    for task in all_tasks:
+        all_args[task] = (task, api_key, base_url, model_name)
+        total+=1
+    pbar = tqdm(total=total)
+    for task in all_args:
+        arguments = all_args[task]
+        all_results[task] = pool.apply_async(infer_local, arguments, callback=lambda _:pbar.update(1))
+    pool.close()
+    pool.join()
+    print("DONE")
+
+    return all_results
+
+
+def infer_local(task, api_key, base_url, model_name):
+    message = [{'role': 'user', 'content': task}]
+    client = OpenAI(api_key=api_key, base_url=base_url)
+
+    gen_params = {
+        GenParams.MAX_NEW_TOKENS: 1000,
+        "return_options": {"input_tokens": True, "generated_tokens": True},
+        'temperature': 0,
+        'extra_headers':  {"RITS_API_KEY": api_key}
+    }
+
+    if client.base_url.host == "api.openai.com":
+        gen_params["max_completion_tokens"] = gen_params.pop("max_new_tokens", None)
+    else:
+        gen_params['max_tokens'] = gen_params.pop("max_new_tokens", None)
+    
+    completion = client.chat.completions.create(
+            messages=message,
+            model=model_name,
+            **gen_params
+        )
+    generated_text = completion.choices[0].message.content
+    return [{"role": "user", "content": task}, {"role": "assistant", "content": generated_text}]
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument("--model")
+    parser.add_argument("--data_path", help="path to the responses to evaluate")
+    parser.add_argument("--split", required=True, choices=['train', 'validation', 'test'])
+    parser.add_argument("--tasks_key", required=True, help="the tasks column name")
+    parser.add_argument("--out_dir")
+    args = parser.parse_args()
+    dataset = OrigData(args.data_path, args.split, args.tasks_key)
+
+    generator = InitGenerationsRITS(args.model, dataset)
+    out_path = generator.get_out_path(args.out_dir)
+    all_results_dict = generate_parallel(generator)
+    generator.dump_results(args.out_path, all_results_dict)
+
+
