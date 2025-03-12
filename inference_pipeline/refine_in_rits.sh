be29689076dac24f4d692ca7985b845d9de773a2
gililior <gili.lior96@gmail.com>
2025-03-12 20:18:09 +0200

    big model run revisions

---
+++ inference_pipeline/refine_in_rits.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+models=("llama3.3-70b" "mistral-large" "deepseek-v3")
+
+init_response_dir="/path/to/repo/wild-if-eval-code/model_predictions"
+out_dir="/path/to/out/dir"
+mkdir -p $out_dir
+
+
+for ((i=0; i<${#models[@]}; i++)); do
+  for ((j=0; j<${#models[@]}; j++)); do
+    generator="${models[i]}"
+    revision_model="${models[j]}"
+    path_to_init_response_generator="${init_response_dir}/${generator}-0shot-wild-if-eval.json"
+    echo "Running combination: generator=$generator, revision_model=$revision_model"
+    python inference_pipeline/big_models_revisions_rits.py \
+      --data_path $path_to_init_response_generator \
+      --model $revision_model \
+      --out_dir out_dir \
+      --tasks_batch_size 200
+  done
+done
